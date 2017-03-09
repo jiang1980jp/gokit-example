09221fcacd7948b842d55e2788250e6784101be2
jiangxuefei <jiangxuefei@jiangcompany.co.jp>
2017-03-10 00:36:44 +0900

    modify second class

--- src/com/myproject/springboot/study/myApplication.java
+++ src/com/myproject/springboot/study/myApplication.java
@@ -4,5 +4,7 @@ package com.myproject.springboot.study;
  * Created by hunter_win7 on 2017/03/10.
  */
 public class myApplication {
-    public void main(String args){}
+    public void main(String args){
+        System.out.print("testlog");
+    }
 }
